db76b760913ea5bd188e6e172e9da91ec3cb9bf1
dennis6935 <dennis.arroyo@upr.edu>
2016-09-24 17:45:25 -0400

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,51 @@
+function compute() {
+    var str = document.getElementById("data").value;
+    var s = str.split(" ");
+    var i=0;
+    var sum = 0;
+    
+    for (i; i < s.length; i++) {
+        sum += parseInt(s[i]);
+    }
+    
+    var avg = sum/s.length;
+    
+    document.getElementById("result").value = "Average = " + avg.toFixed(2);
+}
+
+function computeMedian() {
+    var str = document.getElementById("data").value;
+    var s = str.split(" ");
+    s.sort();
+    var median = 0;
+    
+    if (s.length%2 !== 0) {
+        median = s[(s.length/2)-0.5];
+    }
+    
+    else if (s.length%2 === 0) {
+        var number1 = s.length/2;
+        var number2 = (s.length/2) - 1;
+        median = (parseInt(s[number1]) + parseInt(s[number2]))/2;
+    }
+    
+    document.getElementById("medianResult").value = "Median = " + median;
+}
+
+function computeTavg() {
+    var str = document.getElementById("data").value;
+    var s = str.split(" ");
+    s.sort();
+    
+    var truncatedAvg = 0;
+    var i = 1;
+    var sum = 0;
+    
+    for (i; i < s.length-1; i++) {
+        sum += parseInt(s[i]);
+    }
+    
+    truncatedAvg = sum/(s.length-2);
+    
+    document.getElementById("truncatedResult").value = "Truncated Average = " + truncatedAvg;
+}
